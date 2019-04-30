fff134b590d2522b9e8a99a9b40d6f79c924f393
ken9496 <ngkj777@gmail.com>
2019-04-30 12:07:27 +0800

    made show route show username

--- server.py
+++ server.py
@@ -12,5 +12,10 @@ def show():
     return '<h1>Yo</h1>'
 
 
+@app.route('/user/<username>')
+def shows(username):
+    return f"Hi {username[0:3]}"
+
+
 if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://school.nextacademy.com/courses/full-stack-web-development-bootcamp-with-python/lessons/2947
     app.run()
