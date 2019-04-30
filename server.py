88f6b6ae00fe7ab8265bb2a6f0484886e03d1046
ken9496 <ngkj777@gmail.com>
2019-04-30 12:01:57 +0800

    added shw route

--- server.py
+++ server.py
@@ -7,5 +7,10 @@ def index():
     return '<h1>Why so easy</h1>'
 
 
+@app.route("/another")
+def show():
+    return '<h1>Yo</h1>'
+
+
 if __name__ == '__main__':  # Revisit previous challenge if you're uncertain what this does https://school.nextacademy.com/courses/full-stack-web-development-bootcamp-with-python/lessons/2947
     app.run()
